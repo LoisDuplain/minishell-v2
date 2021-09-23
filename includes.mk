07590725f7b5ff3e08b58df075a3f1da2cc81562
Lois Duplain <lduplain@z3r3p5.42lyon.fr>
2021-09-23 14:03:04 +0200

    Backup Thursday, September 23, 2021 14:02

--- includes.mk
+++ includes.mk
@@ -1,3 +1,3 @@
 INCLUDES += ./includes/minishell.h
-INCLUDES += ./includes/minishell_shell_struct.h
 INCLUDES += ./includes/minishell_shell_func.h
+INCLUDES += ./includes/minishell_shell_struct.h
--- sources.mk
+++ sources.mk
@@ -1,7 +1,7 @@
-SOURCES += sources/shell/destroy_shell.c
-SOURCES += sources/shell/set_line.c
-SOURCES += sources/shell/update_prompt.c
+SOURCES += sources/main.c
 SOURCES += sources/shell/create_shell.c
+SOURCES += sources/shell/destroy_shell.c
 SOURCES += sources/shell/environment/get_env_length.c
 SOURCES += sources/shell/environment/set_env.c
-SOURCES += sources/main.c
+SOURCES += sources/shell/set_line.c
+SOURCES += sources/shell/update_prompt.c
