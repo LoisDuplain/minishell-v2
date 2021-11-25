66524f2eac89c6fedb49cca660ebba128d37b0d1
Loïs Duplain <loisdupl42@gmail.com>
2021-11-25 10:56:44 +0100

    Backup Thursday, November 25, 2021 10:56

--- sources/builtin/exit_builtin.c
+++ sources/builtin/exit_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit_builtin.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lduplain <lduplain@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: lduplain < lduplain@student.42lyon.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 15:08:20 by lduplain          #+#    #+#             */
-/*   Updated: 2021/10/27 15:11:49 by lduplain         ###   ########.fr       */
+/*   Updated: 2021/11/25 10:56:15 by lduplain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@ void	exit_builtin(t_shell *shell, char **args)
 {
 	(void)args;
 	ft_putstr_errnl("exit");
-	ft_putstr("\033[0;32m");
-	ft_putstr("Goodbye :)");
-	ft_putstr_nl("\033[0m");
-	exit_shell(shell, NULL);
+	if (ft_get_string_array_length(args) > 2)
+		put_error("minishell", "exit", "too many arguments");
+	else
+	{
+		// TODO ATOI EXIT CODE
+		ft_putstr("\033[0;32m");
+		ft_putstr("Goodbye :)");
+		ft_putstr_nl("\033[0m");
+		exit_shell(shell, NULL);
+	}
 }
